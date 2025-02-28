3c2fa599d7116a29a3c21811e171f017725fa631
Diegoperezvila <diegoperezvila99@gmail.com>
2025-02-28 19:22:33 +0100

    ver reservas mesas

---
+++ js/verMesas.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+    obtenerPedidosPorUsuario(localStorage.getItem("usuario"));
+});
+
+const obtenerPedidosPorUsuario = async (usuario) => {
+    try {
+        const response = await fetch("https://restaurante-api-sage.vercel.app/api/auth/mesas/filtro", {
+            method: 'POST',  // Usamos POST
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ usuario })  // Enviamos el usuario en el cuerpo de la solicitud
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error al obtener pedidos: ${response.status}`);
+        }
+
+        const pedidos = await response.json();  // Parseamos la respuesta JSON
+        console.log('Pedidos del usuario:', pedidos);
+        mostrarPedidos(pedidos);  // Llamamos a la función para mostrar los pedidos
+    } catch (error) {
+        console.error('Error al hacer fetch:', error);
+    }
+};
+
+const mostrarPedidos = (pedidos) => {
+    const containerPedidos = document.querySelector('.pedidos');
+    containerPedidos.innerHTML = '';  // Limpiamos el contenedor antes de mostrar nuevos pedidos
+
+    if (pedidos.length === 0) {
+        containerPedidos.innerHTML = '<p>No hay pedidos para este usuario.</p>';
+        return;
+    }
+
+    pedidos.forEach(pedido => {
+        console.log(pedido.productos);
+        
+        const pedidoElement = document.createElement('div');
+        pedidoElement.classList.add('pedido-item', 'p-3', 'border', 'rounded', 'w-100');
+        pedidoElement.innerHTML = `
+            <strong>Pedido ID:</strong> ${pedido._id} <br>
+            <strong>Cantidad Personas:</strong> ${pedido.cantidadPersonas} <br>
+            <strong>Fecha:</strong> ${pedido.fecha} <br>
+            <strong>Hora:</strong> ${pedido.hora}
+        `;
+
+        containerPedidos.appendChild(pedidoElement);
+    });
+};
